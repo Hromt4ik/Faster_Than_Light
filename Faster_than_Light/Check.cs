23d24445933e36400949b7ac18a3c8a3522f8ca6
Hromt4ik <97237642+Hromt4ik@users.noreply.github.com>
2023-06-08 23:39:02 +0500

    fgrdg

--- Faster_than_Light/Check.cs
+++ Faster_than_Light/Check.cs
@@ -19,11 +19,8 @@ namespace Faster_than_Light
         }
 
 
-        public static bool Phone(string message)
-        {
-
-
-        }
+        public static bool Phone(string phoneNumber) => Regex.IsMatch(phoneNumber, @"8\d{10}");
+ 
         public static bool Email(string email) => Regex.IsMatch(email, @"[a-zA-Z1-9\-\._] +@[a-z1-9]+(.[a-z1-9] +){ 1,}");
 
     }
