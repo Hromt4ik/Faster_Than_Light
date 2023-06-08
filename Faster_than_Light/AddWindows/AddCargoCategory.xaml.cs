96e71d5346e9cb7e0355e298e821b7322658a08a
Hromt4ik <97237642+Hromt4ik@users.noreply.github.com>
2023-06-09 04:08:58 +0500

    2313

--- Faster_than_Light/AddWindows/AddCargoCategory.xaml.cs
+++ Faster_than_Light/AddWindows/AddCargoCategory.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static Faster_than_Light.NavigateMethods;
 
 namespace Faster_than_Light.AddWindows
 {
@@ -25,18 +26,46 @@ namespace Faster_than_Light.AddWindows
             InitializeComponent();
         }
 
-        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        private void addButton_Click(object sender, RoutedEventArgs e)
         {
+
+            if (Check.Empty(NameBox.Text))
+            {
+                MessageBox.Show("Введите Имя", "Поле не заполнено", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (Check.Empty(CoeffBox.Text))
+            {
+                MessageBox.Show("Введите Коэфициент", "Поле не заполнено", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (!(Check.IsPositivNumber(CoeffBox.Text)))
+            {
+                return;
+            }
+
+
+            if (Check.Empty(CommBox.Text))
+            {
+                MessageBox.Show("Введите Комментарий", "Поле не заполнено", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
             DatabaseControl.AddCargoCategory(new classes.CargoCategory
             {
-                Name = NameBox.Text
+                Name = NameBox.Text,
+                TransportationCoefficient = Convert.ToDecimal(CoeffBox.Text),
+                Comments = CommBox.Text
+            }) ;
 
-            });
-
+            GridStorage.grid.ItemsSource = null;
+            GridStorage.grid.ItemsSource = DatabaseControl.GetCargoСategoryForView();
             Close();
         }
 
-        private void addButton_Click(object sender, RoutedEventArgs e)
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
         {
             Close();
         }
